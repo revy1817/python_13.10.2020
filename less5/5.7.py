c5d841946c0bc26614475327f71610dc80ee73f7
Revy1817 <n7mq7qut5z@gmail.com>
2020-10-30 03:44:17 +0300

    fix

--- less5/5.7.py
+++ less5/5.7.py
@@ -8,10 +8,10 @@ with open("5.7_CompanyInfo.txt", "r", encoding="UTF-8") as company_info:
     for line in lines:
         line_split = line.split()
         key = line_split[0]
-        quantity += 1
         profit_company = int(line_split[2]) - int(line_split[3][:-1])
         if profit_company > 0:
             total_profit += profit_company
+            quantity += 1
         company_result[key] = profit_company
     total_profit_dict["average_profit"] = total_profit / quantity
 company_info_dict = [company_result, total_profit_dict]
